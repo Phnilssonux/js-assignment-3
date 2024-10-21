585d5c63721aa46406dc3c97bb07019dd92f0c61
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-10-22 01:43:48 +0200

    Commented requirements

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+Write a isValidPassword function 
+It accepts 2 arguments: password and username 
+Password must: 
+- be atleast 8 characters 
+- cannot contain spaces 
+- cannot contain the username 
+If all requirements are met, return true, 
+otherwise return false  
+*/
+
